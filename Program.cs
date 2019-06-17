23266a82b2e7c4794fb08fe31d24d176909bcd53
Will Murchison <wmurch@hotmail.com>
2019-06-16 21:45:41 -0400

    added the delete, update and count methods

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace SafariVacations
             while (input != "quit")
             {
 
-                Console.WriteLine("You want to (view), (add),(update) or (remove) Animals ...");
+                Console.WriteLine("You want to (view), (add),(update),(count), or (remove) Animals ...");
                 Console.WriteLine(":============================================================: ");
                 input = Console.ReadLine();
 
@@ -36,41 +36,128 @@ namespace SafariVacations
                 //Update how many times you have seen animals and where
                 else if (input == "update")
                 {
+                    Console.WriteLine("Which record would you like to update your sightings? : ");
+                    input = Console.ReadLine();
 
+                    var data = input;
+                    var updateAnimal = db.Animals.FirstOrDefault(fd => fd.Species == data);
+                    if (updateAnimal != null)
+                    {
+                        Console.WriteLine("Please enter the updated numbers of sightings: ");
+                        input = Console.ReadLine();
+
+                        var seenData = int.Parse(input);
+                        updateAnimal.CountOfTimesSeen = seenData;
+                        Console.WriteLine($"{updateAnimal.Species},{updateAnimal.CountOfTimesSeen}, {updateAnimal.LocationOfLastSeen}");
+
+                    }
 
                 }
                 // Display All Animals
                 else if (input == "view")
+                // Display all animals seen in the Jungle
                 {
-                    var allAnimals = db.Animals.OrderBy(o => o.Species);
-                    foreach (var animal in allAnimals)
+                    Console.WriteLine("If you want to view all animals type (all) if you want to see an animal by location type (desert),(forest) or (jungle) ty : ");
+                    input = Console.ReadLine();
+                    if (input == "all")
                     {
-                        Console.WriteLine($"{animal.Species}, {animal.LocationOfLastSeen}");
+                        var allAnimals = db.Animals.OrderBy(o => o.Species);
+                        foreach (var animal in allAnimals)
+                            Console.WriteLine($"{animal.Species},{animal.CountOfTimesSeen}, {animal.LocationOfLastSeen}");
+                    }
+                    else if (input == "desert")
+                    {
+                        var allAnimals = db.Animals.OrderBy(o => o.Species);
+                        foreach (var animal in allAnimals)
+                        {
+                            if (animal.LocationOfLastSeen == "desert")
+                            {
+                                Console.WriteLine($"{animal.Species},{animal.CountOfTimesSeen}, {animal.LocationOfLastSeen}");
+                            }
+                        }
+
+                    }
+                    else if (input == "forest")
+                    {
+                        var allAnimals = db.Animals.OrderBy(o => o.Species);
+                        foreach (var animal in allAnimals)
+                        {
+                            if (animal.LocationOfLastSeen == "forest")
+                            {
+                                Console.WriteLine($"{animal.Species},{animal.CountOfTimesSeen}, {animal.LocationOfLastSeen}");
+                            }
+                        }
+
+                    }
+                    else if (input == "jungle")
+                    {
+                        var allAnimals = db.Animals.OrderBy(o => o.Species);
+                        foreach (var animal in allAnimals)
+                        {
+                            if (animal.LocationOfLastSeen == "desert")
+                            {
+                                Console.WriteLine($"{animal.Species},{animal.CountOfTimesSeen}, {animal.LocationOfLastSeen}");
+                            }
+                        }
+
                     }
                 }
                 // Remove all Animals that I have seen in the Desert
                 else if (input == "remove")
                 {
-                    //Console.WriteLine("Type the name of the Animal you want to remove ");
-                    //input = Console.ReadLine();
-                    //var data = input;
-                    var delAnimal = db.Animals.Where(varmint => varmint.LocationOfLastSeen == "Desert");
-                    db.Animals.RemoveRange(delAnimal);
-                    db.SaveChanges();
+                    Console.WriteLine("Remove all animals from a location type (desert),(forest) or (jungle):  ");
+                    input = Console.ReadLine();
+                    var data = input;
+                    if (input == "desert")
+                    {
+                        var delAnimal = db.Animals.Where(varmint => varmint.LocationOfLastSeen == "desert");
+                        db.Animals.RemoveRange(delAnimal);
+                        db.SaveChanges();
+                    }
+                    else if (input == "forest")
+                    {
+                        var delAnimal = db.Animals.Where(varmint => varmint.LocationOfLastSeen == "forest");
+                        db.Animals.RemoveRange(delAnimal);
+                        db.SaveChanges();
+                    }
+                    else if (input == "jungle")
+                    {
+                        var delAnimal = db.Animals.Where(varmint => varmint.LocationOfLastSeen == "jungle");
+                        db.Animals.RemoveRange(delAnimal);
+                        db.SaveChanges();
+                    }
+
                     //Console.WriteLine("Not ready yet but working on it");
+                }
+                else if (input == "count")
+                {
+                    // Get the CountOfTimesSeen of Lions Tigers and Bears   
+                    Console.WriteLine("To get sum of all animals seen type (total) but if you only want the total of Lions, Tiger and Bears type (ohmy)  :  ");
+                    input = Console.ReadLine();
+                    var data = input;
+
+                    // Add all the CountOfTimeSeen and get a total number of animals seen
+                    if (input == "total")
+                    {
+                        var countAnimals = db.Animals.Sum(num => num.CountOfTimesSeen);
+                        Console.WriteLine($"The sum for all sightings are {countAnimals}");
+
+                    }
+                    else if (input == "ohmy")
+                    {
+                        //var allAnimals = db.Animals.OrderBy(o => o.Species);
+                        var countAnimals = db.Animals.Where(w => w.Species == "Tiger" || w.Species == "lion" || w.Species == "bear").Sum(num => num.CountOfTimesSeen);
+                        Console.WriteLine($"Hey Dorothy you only have to deal with {countAnimals} dangerous animals on the yellow brick road");
+                    }
+
                 }
 
 
-
-                // Display all animals seen in the Jungle
-
-
-                // Add all the CountOfTimeSeen and get a total number of animals seen
-
-                // Get the CountOfTimesSeen of Lions Tigers and Bears   
             }
 
         }
+
     }
 }
+
 
